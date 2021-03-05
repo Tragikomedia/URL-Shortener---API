236aa1dfb356187ea323d363f29a0215b34e5503
Tragikomedia <kamilk9625@gmail.com>
2021-03-05 14:56:25 +0100

    Added tests for URI generating.

---
+++ __tests__/helpers/generateURI.js
@@ -0,0 +1,24 @@
+const { generateUniqueURI } = require('../../routes/helpers/generateURI');
+
+const db = require('../../config/db');
+const uriStorage = require('../../config/uriStorage');
+const UriStorage = require('../../models/uriStorage');
+
+beforeAll( async done => {
+    await db.connect()
+    await uriStorage.initialize();
+    done();
+});
+
+afterAll(done => {
+    db.disconnect();
+    done();
+});
+
+describe('Unique URI generation', () => {
+    it('Given access to storage, should generate unique URI', async () => {
+        const storage = await UriStorage.findOne();
+        const uri = await generateUniqueURI(storage);
+        expect(uri).toMatch(/^[a-z0-9]{7}$/);
+    });
+});
