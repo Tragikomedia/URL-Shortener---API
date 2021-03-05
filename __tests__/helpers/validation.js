fc7d142852c4a205d309beb38cb096629118a5de
Tragikomedia <kamilk9625@gmail.com>
2021-03-05 17:31:43 +0100

    Added URI validation tests.

--- __tests__/helpers/validation.js
+++ __tests__/helpers/validation.js
@@ -1,4 +1,4 @@
-const { validURL } = require('../../routes/helpers/validation');
+const { validURL, validInternalURI } = require('../../routes/helpers/validation');
 
 describe('URL Validation - invalid', () => {
     it('Given url with space, should equal false', () => {
@@ -35,5 +35,24 @@ describe('URL Validation - valid', () => {
     it('Given short url with additional path, should equal true', () => {
         const valid = "reddit.com/r/NintendoSwitch/";
         expect(validURL(valid)).toBe(true);
+    });
+});
+
+describe('URI validation', () => {
+    it('Given a valid URI, should return true', () => {
+        const uri = 'm7rr3ta';
+        expect(validInternalURI(uri)).toBe(true);
+    });
+    it('Given too short URI, should return false', () => {
+        const uri = 'ab8';
+        expect(validInternalURI(uri)).toBe(false);
+    });
+    it('Given too long URI, should return false', () => {
+        const uri = 'm7rr3tab';
+        expect(validInternalURI(uri)).toBe(false);
+    });
+    it('Given URI with illegal symbols, should return false', () => {
+        const uri = 'm7%r3ta';
+        expect(validInternalURI(uri)).toBe(false);
     });
 });
