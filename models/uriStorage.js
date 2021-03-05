dd1c56df1aae8b74de1db10fa4d624832690a0c8
Tragikomedia <kamilk9625@gmail.com>
2021-03-05 13:44:28 +0100

    Created model for the storage of uri ids.

---
+++ models/uriStorage.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+
+const uriStorageSchema = new Schema({
+    uris: {
+        type: [String],
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    modifiedAt: {
+        type: Date,
+    }
+});
+
+module.exports = model('UriStorage', uriStorageSchema);
