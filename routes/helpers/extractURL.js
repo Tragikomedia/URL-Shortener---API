6d965b5e08a6b3907e056a5b5f878cbfc120dc45
Tragikomedia <kamilk9625@gmail.com>
2021-03-05 20:21:04 +0100

    Simplified extracting regexp.

--- routes/helpers/extractURL.js
+++ routes/helpers/extractURL.js
@@ -1,4 +1,4 @@
-const shortUrlRegExp = /[a-zA-Z0-9@:%._\\+~#?&/=]{2,256}\.[a-z]{2,6}([-a-zA-Z0-9@:%._\\+~#?&//=]*)$/
-const extractTargetUrl = url => shortUrlRegExp.exec(url)[0];
+const shortUrlRegExp = /^((http|https):\/\/)?(www.)?(.*)$/
+const extractTargetUrl = url => shortUrlRegExp.exec(url)[4];
 
 module.exports = { extractTargetUrl };
