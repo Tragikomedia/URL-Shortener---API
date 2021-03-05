00b231efaaf9e3ca2e3e3aa8519f771f64075407
Tragikomedia <kamilk9625@gmail.com>
2021-03-05 20:21:40 +0100

    Fixed the behavior of links starting with http.

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.get('/:id', async (req, res) => {
     const link = await Link.findOne({shortURI: id});
     if (!link) return res.json({error: 'Link does not exist'});
     const url = link.targetURL;
-    res.redirect(url);
+    res.redirect(`https://${url}`);
 });
 
 
