8eca3d9f4365937384cc9ab6fc9c7d8004cefe9e
Tragikomedia <kamilk9625@gmail.com>
2021-03-06 17:06:48 +0100

    Added general user model.

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    externalId: {
+        type: String,
+        required: true
+    },
+    provider: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = model('User', userSchema);
