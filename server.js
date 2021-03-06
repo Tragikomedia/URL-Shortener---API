c55f767e7cd5b7a5dcef38e6b27becb11aad6875
Tragikomedia <kamilk9625@gmail.com>
2021-03-06 17:04:19 +0100

    Added view engine necessary for 3rd party auth.

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 
+// View engine used for 3rd party auth
+const mustacheExpress = require('mustache-express');
+app.engine('html', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/public');
+
 // Middlewares
 const cors = require('cors');
+const passport = require('./config/passport');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(passport.initialize());
 
 // Routes
 const apiRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 app.use('/', apiRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
