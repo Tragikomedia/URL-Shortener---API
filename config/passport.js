9b74dc16d69b1790387380bc5bc238c322b8ec45
Tragikomedia <kamilk9625@gmail.com>
2021-03-07 15:28:11 +0100

    Added Google strategy, moved OAuth strategy logic into separate function.

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const JWTStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
@@ -10,23 +11,34 @@ const facebookStrategy = new FacebookStrategy({
     clientSecret: process.env.FB_CLIENT_SECRET,
     callbackURL: process.env.FB_CALLBACK
 },
-async function(accessToken, refreshToken, profile, done) {
-    let user;
-    try {
-        user = await User.findOne({externalId: profile.id, provider: 'Facebook'});
-        if (!user) {
-            user = new User({
-                externalId: profile.id,
-                provider: 'Facebook',
-                name: profile.displayName
-            });
-            await user.save();
+setUpStrategy('Facebook'));
+
+// Google Strategy
+const googleStrategy = new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+}, 
+setUpStrategy('Google'));
+
+function setUpStrategy(provider) {
+    return async function(accessToken, refreshToken, profile, done) {
+        try {
+            let user = await User.findOne({externalId: profile.id, provider});
+            if (!user) {
+                user = new User({
+                    externalId: profile.id,
+                    provider,
+                    name: profile.displayName
+                });
+                await user.save();
+            }
+            done(null, user);
+        } catch(error) {
+            done(error);
         }
-        done(null, user);
-    } catch (error) {
-        done(error);
-    }
-});
+    };
+}
 
 // JWT Strategy
 const opts = {
@@ -46,6 +58,7 @@ const jwtStrategy = new JWTStrategy(opts, async function(jwt_payload, done) {
 });
 
 passport.use(facebookStrategy);
+passport.use(googleStrategy);
 passport.use(jwtStrategy);
 
 module.exports = passport;
