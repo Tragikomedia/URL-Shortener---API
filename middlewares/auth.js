f23f9a8f178e08707b4feebc1275413967fd0318
Tragikomedia <kamilk9625@gmail.com>
2021-03-07 13:14:44 +0100

    Added attemptAuthentication function to allow both anonymous and authenticated users.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,4 +2,14 @@ const passport = require("passport");
 
 const authenticateUser = (req, res, next) => passport.authenticate('jwt', {session: false})(req, res, next);
 
-module.exports = { authenticateUser };
+// Use custom callback to override default behaviour and let unauthenticated users proceed
+const attemptAuthetication = (req, res, next) => passport.authenticate('jwt', {session: false}, function(err, user, info) {
+    if (user) {
+        req.logIn(user, function(err) {
+            if (err) return next(err);
+            return next();
+        });
+    }
+    next();
+})(req, res, next);
+module.exports = { authenticateUser, attemptAuthetication };
