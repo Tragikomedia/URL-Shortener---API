0b5da042d73c8126222279f84d4c0703b94d0b8f
Tragikomedia <kamilk9625@gmail.com>
2021-03-07 13:30:17 +0100

    Simplified attemptAuthentication's logic.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,11 +5,9 @@ const authenticateUser = (req, res, next) => passport.authenticate('jwt', {sessi
 // Use custom callback to override default behaviour and let unauthenticated users proceed
 const attemptAuthetication = (req, res, next) => passport.authenticate('jwt', {session: false}, function(err, user, info) {
     if (user) {
-        req.logIn(user, function(err) {
-            if (err) return next(err);
-            return next();
-        });
+        req.user = user;
     }
     next();
 })(req, res, next);
+
 module.exports = { authenticateUser, attemptAuthetication };
