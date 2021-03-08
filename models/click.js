e4bb8e53d34787a9f9d81db4868f75dba1fe6dbd
Tragikomedia <kamilk9625@gmail.com>
2021-03-08 17:09:21 +0100

    Added a method to create new Clicks from requests.

--- models/click.js
+++ models/click.js
@@ -13,4 +13,16 @@ const clickSchema = new Schema({
     }
 });
 
+// Static
+const Click = require('../models/click');
+const requestIp = require('request-ip');
+
+clickSchema.static.fromReq = function(req) {
+    const stats = {};
+    stats.ip = requestIp.getClientIp(req);
+    if (req.headers?.referer) stats.referer = req.headers.referer;
+    const click = new Click(stats);
+    return click;
+}
+
 module.exports = model('Click', clickSchema);
