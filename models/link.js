ee86dfc4f97b309c7f8dc4deed907b3db32387ef
Tragikomedia <kamilk9625@gmail.com>
2021-03-08 14:27:08 +0100

    Added fields relating to stats and expiration: expired, expiresAt, maxClicks, clicks.

--- models/link.js
+++ models/link.js
@@ -13,6 +13,21 @@ const linkSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
+    expired: {
+        type: Boolean,
+        default: false
+    },
+    expiresAt: {
+        type: Date,
+    },
+    maxClicks: {
+        type: Number,
+    },
+    clicks: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Click',
+        default: []
+    },
     createdAt: {
         type: Date,
         default: Date.now
