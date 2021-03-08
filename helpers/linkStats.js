298d399e7a10ad80c89ea8ccdeb6c916d70d37ae
Tragikomedia <kamilk9625@gmail.com>
2021-03-08 17:11:02 +0100

    Created a function used to update clicks referenced by link object.

---
+++ helpers/linkStats.js
@@ -0,0 +1,15 @@
+const Click = require('../models/click');
+
+async function updateLinkStats(req, link) {
+    const click = Click.fromReq(req);
+    link.clicks.push(click.id);
+    try {
+        // Link has priority - important for expiration purposes
+        await link.save();
+        await click.save();
+    } catch(error) {
+        return {error};
+    }
+}
+
+module.exports = { updateLinkStats };
