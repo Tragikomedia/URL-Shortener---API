fb20712065417dc7965fe4f02d7ca119deec77e4
Tragikomedia <kamilk9625@gmail.com>
2021-03-09 16:58:10 +0100

    Added /user/:id route for getting detailed info about given link.

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Link = require('../models/link');
 const { authenticateUser } = require('../middlewares/auth');
-const { extractData } = require('../helpers/linkData');
+const { extractData, getLinkData } = require('../helpers/linkData');
 
 // GET /user/all
 // @desc Get all links posted by the user
@@ -13,6 +13,16 @@ router.get('/all', authenticateUser, async (req, res) => {
         linksData
     };
     res.json(message);
+});
+
+// GET /user/:id
+// @desc Get a particular link posted by the user
+router.get('/:id', authenticateUser, async (req, res) => {
+    const { id } = req.params;
+    const { error, link } = await Link.findInUserLinks(req.user, id);
+    if (error) return res.json({error});
+    const linkData = await getLinkData(link, 'exhaustive');
+    res.json({linkData});
 });
 
 module.exports = router;
