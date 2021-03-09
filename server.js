c917500aecf699e6160c715289df22bb5c9df27a
Tragikomedia <kamilk9625@gmail.com>
2021-03-09 18:23:28 +0100

    Added method-override package.

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ app.set('views', __dirname + '/views');
 // Middlewares
 const cors = require('cors');
 const passport = require('./config/passport');
+const methodOverride = require('method-override');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(passport.initialize());
+app.use(methodOverride('_method'));
 
 // Routes
 const apiRouter = require('./routes/api');
