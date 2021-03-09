9342e9916907cbbd92cb2f77bd5ba3a10f8e9435
Tragikomedia <kamilk9625@gmail.com>
2021-03-09 17:36:32 +0100

    Changed test database url to avoid conflicts with other projects.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-    let mongoURL = process.env.NODE_ENV !== 'test' ? process.env.DATABASE_URL : "mongodb://localhost/test";
+    let mongoURL = process.env.NODE_ENV !== 'test' ? process.env.DATABASE_URL : "mongodb://localhost/test1";
     const db = mongoose.connection;
     db.on('error', error => console.error(error));
     db.once('open', () => console.log('Connected to the DB...'));
