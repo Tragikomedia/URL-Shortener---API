b2af89b0ccd234ee0edd6c96a1fd9691ddf0c070
Tragikomedia <kamilk9625@gmail.com>
2021-03-29 13:05:37 +0200

    Moved calling dotenv from here to env module.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
+const env = require('./config/env');
 const db = require('./config/db');
 const app = require('./server');
 const uriStorage = require('./config/uriStorage');
 
 const runServer = async () => {
-    await db.connect();
-    await uriStorage.initialize();
-    app.listen(process.env.PORT || 3000);
-}
+  await db.connect();
+  await uriStorage.initialize();
+  app.listen(env.PORT);
+};
 
 runServer();
