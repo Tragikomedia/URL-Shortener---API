d4ca2cb00efa6ecabf750504e70c93f9d2b1089b
Tragikomedia <kamilk9625@gmail.com>
2021-03-30 17:21:26 +0200

    Added jsdocs-style comments for swagger.

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,95 @@ const { attemptAuthetication } = require('../middlewares/auth');
 const { updateLinkStats } = require('../helpers/linkStats');
 const Link = require('../models/link');
 
+/**
+ * @swagger
+ * components:
+ *  securitySchemes:
+ *    bearerAuth:
+ *      type: http
+ *      scheme: bearer
+ *      bearerFormat: JWT
+ *  schemas:
+ *    Link:
+ *      type: object
+ *      required:
+ *        - uri
+ *      properties:
+ *        uri:
+ *          type: string
+ *          example: rDf4w21
+ *          description: A short uri you can attach to api server's address to use the link e.g. server.com/uri
+ *    UserInput:
+ *      type: object
+ *      required:
+ *        -url
+ *      properties:
+ *        url:
+ *          type: string
+ *          example: www.github.com
+ *          format: uri
+ *          description: The link you want to have shortened
+ *        options:
+ *          type: object
+ *          description: Bonus expiration options
+ *          properties:
+ *            expiresAt:
+ *              type: string
+ *              format: date
+ *              example: 2077-10-23
+ *              description: Date before which the link is active
+ *            maxClicks:
+ *              type: integer
+ *              minimum: 1
+ *              description: Maximum number of times the link can be clicked before it expires
+ *    Error:
+ *      type: object
+ *      required:
+ *        -error
+ *      properties:
+ *        error:
+ *          type: string
+ *          description: Error message
+ */
+ 
 // POST /
-// @desc Post a link to be shortened, receive URI
+// @desc Post link you want to have shortened
+/** 
+ * @swagger
+ * /:
+ *  post:
+ *    tags:
+ *      - index
+ *    summary: Post link that you want to shorten
+ *    description: Authorization is not obligatory, but adding a valid JWT makes links associated with the user.
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/UserInput'
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      201:
+ *        description: Returns a uri you can attach to server's address to get redirected to the original link's destination
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Link'
+ *      400:
+ *        description: Provided link is invalid
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#components/schemas/Error'
+ *      500:
+ *        description: Server problem
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#components/schemas/Error'
+ */
 router.post('/', attemptAuthetication, async (req, res) => {
   const { url } = req.body;
   if (!validURL(url)) return res.status(400).json({ error: 'Invalid URL' });
@@ -22,10 +109,34 @@ router.post('/', attemptAuthetication, async (req, res) => {
 
 // GET /:id
 // @desc Access URI, get redirected to the stored URL
+/**
+ * @swagger
+ * /{id}:
+ *   get:
+ *     tags:
+ *      - index
+ *     summary: Use uri id you obtained from POST / to get redirected to your desired destination
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         type: string
+ *         required: true
+ *         description: Uri id of a link stored in the database
+ *     responses:
+ *       302:
+ *         description: Get redirected to the location specified by link corresponding to the provided uri id
+ *       404:
+ *         description: The uri doesn't correspond to any link stored in the database
+ *         content:
+ *          text/html:
+ *            schema:
+ *              type: string
+ *              example: Error - some error text
+ */
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const { error, link } = await Link.findByUri(id);
-  if (error) return res.render('error.html', { error });
+  if (error) return res.status(404).render('error.html', { error });
   const url = link.targetURL;
   res.redirect(`https://${url}`);
   // Useful only for stats or click-related expiration
