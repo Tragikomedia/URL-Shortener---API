1b170e3ee8795437fb69496a90762a728ad52136
Tragikomedia <kamilk9625@gmail.com>
2021-04-07 19:19:04 +0200

    Improved the way db is cleaned.

--- config/db.js
+++ config/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const env = require('./env');
 const logger = require('../helpers/logger');
+const Link = require('../models/link');
+const User = require('../models/user');
+const Click = require('../models/click');
+const UriStorage = require('../models/uriStorage');
 
 async function connect() {
   try {
@@ -24,4 +28,13 @@ async function disconnect() {
   await mongoose.connection.close?.();
 }
 
-module.exports = { connect, disconnect };
+async function clean() {
+  if (env.NODE_ENV === 'test') {
+    await Link.deleteMany({});
+    await Click.deleteMany({});
+    await User.deleteMany({});
+    await UriStorage.deleteMany({});
+  }
+}
+
+module.exports = { connect, disconnect, clean };
