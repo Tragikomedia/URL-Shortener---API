059f791f5ad9f3e6c495aca0315651d900f346c1
Tragikomedia <kamilk9625@gmail.com>
2021-04-07 22:19:36 +0200

    Added a function extracting updates params and reworked blank property removal.

--- helpers/linkParams.js
+++ helpers/linkParams.js
@@ -1,17 +1,46 @@
 const { getUri } = require('../helpers/generateURI');
 const { extractTargetUrl } = require('../helpers/extractURL');
 
+const allAreUndefined = (arr) => arr.every((el) => typeof el === 'undefined');
+
+const validateParams = ({ expired, expiresAt, maxClicks }) => {
+  const error = { error: 'Invalid parameters' };
+  if (allAreUndefined([expired, expiresAt, maxClicks])) return error;
+  if (expired && typeof expired !== 'boolean') return error;
+  if (maxClicks && !(typeof maxClicks === 'number' && maxClicks > 0))
+    return error;
+  if (expiresAt && !!isNaN(new Date(expiresAt))) return error;
+};
+
+const removeEmptyPairs = (obj) => {
+  const cleanObj = {};
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] !== 'undefined') cleanObj[key] = obj[key];
+  });
+  return cleanObj;
+};
+
 async function getLinkParams(req) {
   const { error, shortURI } = await getUri();
   if (error) return { error: 'Unique ID could not have been generated.' };
   const { url, options } = req.body;
-  const params = {};
+  let params = {};
+  if (options) {
+    const err = validateParams(options);
+    if (err) return err;
+    params = { ...params, ...removeEmptyPairs(options) };
+  }
   params.shortURI = shortURI;
   params.targetURL = extractTargetUrl(url);
-  if (options?.maxClicks) params.maxClicks = options.maxClicks;
-  if (options?.expiresAt) params.expiresAt = options.expiresAt;
   if (req.user) params.user = req.user.id;
   return { params };
 }
 
-module.exports = { getLinkParams };
+function getUpdateParams(req) {
+  const { expired, expiresAt, maxClicks } = req.body;
+  const updateObj = removeEmptyPairs({ expired, expiresAt, maxClicks });
+  const error = validateParams(updateObj);
+  return { updateObj, error };
+}
+
+module.exports = { getLinkParams, getUpdateParams };
