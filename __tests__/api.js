74b2f20da64dbe250ad21a12fafd0d5673dd2ffb
Tragikomedia <kamilk9625@gmail.com>
2021-04-07 22:18:45 +0200

    Added tests for PUT /user/links/:id

--- __tests__/api.js
+++ __tests__/api.js
@@ -208,10 +208,6 @@ describe('GET /user/links/:id', () => {
     await link.save();
     await request.get(`/${shortURI}`);
     await request.get(`/${shortURI}`);
-    // const click1 = new Click();
-    // const click2 = new Click();
-    // link.clicks.push(click1.id);
-    // link.clicks.push(click2.id);
     const token = signJWT(user);
     const res = await request
       .get(`/user/links/${shortURI}`)
@@ -248,8 +244,232 @@ describe('GET /user/links/:id', () => {
   });
 });
 
+describe('PUT /user/:id', () => {
+  it('Given a request demanding expiration, the link should expire', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const shortURI = 'oht6652';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user.id,
+    });
+    await link.save();
+    const preRes = await request.get(`/${shortURI}`);
+    expect(preRes.status).toBe(302);
+    const res = await request
+      .put(`/user/links/${shortURI}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ expired: true });
+    expect(res.status).toBe(204);
+    const postRes = await request.get(`/${shortURI}`);
+    expect(postRes.status).toBe(404);
+  });
+  it('Given a request revoking expiration without changing other conditions, the link should still be expired', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const shortURI = 'oht6652';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user.id,
+      maxClicks: 1,
+    });
+    await link.save();
+    const firstRes = await request.get(`/${shortURI}`);
+    expect(firstRes.status).toBe(302);
+    const secondRes = await request.get(`/${shortURI}`);
+    expect(secondRes.status).toBe(404);
+    const putRes = await request
+      .put(`/user/links/${shortURI}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ expired: false });
+    expect(putRes.status).toBe(204);
+    const thirdRes = await request.get(`/${shortURI}`);
+    expect(thirdRes.status).toBe(404);
+  });
+  it('Given a request changing expiration date and expired property, the date should be changed and link active', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const shortURI = 'oht6652';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user.id,
+      maxClicks: 1,
+      expiresAt: new Date('1999'),
+    });
+    await link.save();
+    const firstRes = await request.get(`/${shortURI}`);
+    expect(firstRes.status).toBe(404);
+    const putRes = await request
+      .put(`/user/links/${shortURI}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ expired: false, expiresAt: new Date('2037') });
+    expect(putRes.status).toBe(204);
+    const secondRes = await request.get(`/${shortURI}`);
+    expect(secondRes.status).toBe(302);
+  });
+  it('Given a request changing maxClicks and expired property, maxClicks should be changed and link active', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const shortURI = 'oht6652';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user.id,
+      maxClicks: 1,
+    });
+    await link.save();
+    const firstRes = await request.get(`/${shortURI}`);
+    expect(firstRes.status).toBe(302);
+    const secondRes = await request.get(`/${shortURI}`);
+    expect(secondRes.status).toBe(404);
+    const putRes = await request
+      .put(`/user/links/${shortURI}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ expired: false, maxClicks: 10 });
+    expect(putRes.status).toBe(204);
+    const thirdRes = await request.get(`/${shortURI}`);
+    expect(thirdRes.status).toBe(302);
+  });
+  it('Given a request with no body, should receive status 400', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const shortURI = 'oht6652';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user.id,
+    });
+    await link.save();
+    const res = await request
+      .put(`/user/links/${shortURI}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`);
+    expect(res.status).toBe(400);
+  });
+  it('Given a request with malformatted date, should receive status 400', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const shortURI = 'oht6652';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user.id,
+    });
+    await link.save();
+    const res = await request
+      .put(`/user/links/${shortURI}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ expiresAt: 'zgroza' });
+    expect(res.status).toBe(400);
+  });
+  it('Given a request with malformatted maxClicks, should receive status 400', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const shortURI = 'oht6652';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user.id,
+    });
+    await link.save();
+    const res = await request
+      .put(`/user/links/${shortURI}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ maxClicks: 'buba' });
+    expect(res.status).toBe(400);
+  });
+  it('Given a request with malformatted expired property, should receive status 400', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const shortURI = 'oht6652';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user.id,
+    });
+    await link.save();
+    const res = await request
+      .put(`/user/links/${shortURI}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ expired: 'lelum polelum' });
+    expect(res.status).toBe(400);
+  });
+  it('Given a request made by an unauthorized user, should receive status 401', async () => {
+    const res = await request
+      .put('/user/links/aabbcce')
+      .set('Content-Type', 'application/json')
+      .send({ expired: true });
+    expect(res.status).toBe(401);
+  });
+  it('Given a request pointing at non-existing link, should receive status 404', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const res = await request
+      .put('/user/links/wrongli')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ expired: true });
+    expect(res.status).toBe(404);
+  });
+});
+
 describe('DELETE /user/:id', () => {
-  it('Given an existing uri and a user, should delete link and return 204', async () => {
+  it('Given an existing uri and a user when using method override, should delete link and return 204', async () => {
     const user = new User({
       externalId: 'cc32bee4',
       provider: 'Facebook',
@@ -272,6 +492,29 @@ describe('DELETE /user/:id', () => {
     const foundLink = await Link.findById(link.id);
     expect(foundLink).toBeFalsy();
   });
+  it('Given an existing uri and a user when directly using delete, should delete link and return 204', async () => {
+    const user = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user.save();
+    const token = signJWT(user);
+    const shortURI = 'oht665';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user.id,
+    });
+    await link.save();
+    const res = await request
+      .delete(`/user/links/${shortURI}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`);
+    expect(res.status).toBe(204);
+    const foundLink = await Link.findById(link.id);
+    expect(foundLink).toBeFalsy();
+  });
   it('Given an non-existent uri and a user, should return 204', async () => {
     const user = new User({
       externalId: 'cc32ber4',
@@ -280,12 +523,42 @@ describe('DELETE /user/:id', () => {
     });
     await user.save();
     const token = signJWT(user);
-    const shortURI = 'ohp665';
+    const shortURI = 'ohp6652';
     const res = await request
       .post(`/user/links/${shortURI}?_method=DELETE`)
       .set('Content-Type', 'application/json')
       .set('Authorization', `Bearer ${token}`);
     expect(res.status).toBe(204);
+  });
+  it('Given an existing uri posted by a one user, another user should not be able to delete it', async () => {
+    const user1 = new User({
+      externalId: 'cc32bee4',
+      provider: 'Facebook',
+      name: 'Marek Cukierberg',
+    });
+    await user1.save();
+    const shortURI = 'oht6651';
+    const link = new Link({
+      targetURL: 'vikop.ru',
+      shortURI,
+      user: user1.id,
+    });
+    await link.save();
+    const user2 = new User({
+      externalId: 'c5930340',
+      provider: 'Facebook',
+      name: 'Paulo Dybala',
+    });
+    await user2.save();
+    const token = signJWT(user2);
+    const res = await request
+      .post(`/user/links/${shortURI}?_method=DELETE`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`);
+    // Status 204 because alien user should not know if such a link exists
+    expect(res.status).toBe(204);
+    const foundLink = await Link.findById(link.id);
+    expect(foundLink).toBeTruthy();
   });
 });
 
